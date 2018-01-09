5e20b54de32083f417e93a064a4665a1f089a9f6
zzz <897035440@qq.com>
2018-01-09 19:35:47 +0800

    添加错误输入处理

--- open-source/exp.py
+++ open-source/exp.py
@@ -27,73 +27,83 @@ def filters():
         return render_template('search.html')
     else:
         score=request.form['score']
-        url="http://oscar-lab.org/puzzle.html"
-        req=requests.get(url)
-        soup=bs4.BeautifulSoup(req.text)
-        tbody=soup.table.tbody
-        con = sqlite3.connect("puzzle.db")
-        cursor = con.cursor()
-        cursor.execute("drop table if exists puzzle")
-        cursor.execute("create table if not exists puzzle (id integer primary key,IDs text,Score text,Title text)")
+        if int(score) not in range(1,10):
+            return render_template('validator.html')
+        else:
+            url="http://oscar-lab.org/puzzle.html"
+            req=requests.get(url)
+            soup=bs4.BeautifulSoup(req.text)
+            tbody=soup.table.tbody
+            con = sqlite3.connect("puzzle.db")
+            cursor = con.cursor()
+            cursor.execute("drop table if exists puzzle")
+            cursor.execute("create table if not exists puzzle (id integer primary key,IDs text,Score text,Title text)")
 
-        for tr in tbody.find_all("tr"):
-            if tr.find_all("td")[1].text==str(score):
-                IDs=tr.find_all("td")[0].text
-                Score=tr.find_all("td")[1].text
-                Title=tr.find_all("td")[2].a.text
-                print("%s\n%s\n%s" % (IDs,Score,Title))
-                cursor.execute("insert into puzzle (IDs, Score, Title) values (?, ?, ?)", (IDs, Score, Title))
+            for tr in tbody.find_all("tr"):
+                if tr.find_all("td")[1].text==str(score):
+                    IDs=tr.find_all("td")[0].text
+                    Score=tr.find_all("td")[1].text
+                    Title=tr.find_all("td")[2].a.text
+                    print("%s\n%s\n%s" % (IDs,Score,Title))
+                    cursor.execute("insert into puzzle (IDs, Score, Title) values (?, ?, ?)", (IDs, Score, Title))
         
-        con.commit()
-        con.close()
-        return render_template('finish.html')
+            con.commit()
+            con.close()
+            return render_template('finish.html')
 
 @app.route("/mooc",methods=['POST', 'GET'])
 def mooc():
     if request.method == 'GET':
         return render_template('search.html')
     else:
+        classname = request.form['classes']
         url="https://deathwingi.github.io/"
         req=requests.get(url)
         soup=bs4.BeautifulSoup(req.text)
         table=soup.body.table
-        con = sqlite3.connect("mooc.db")
-        cursor = con.cursor()
-        cursor.execute("drop table if exists mooc")
-        cursor.execute("create table if not exists mooc (id integer primary key,area text,time text,sno text,classes text,cnum text,cno text,cname text)")
-        classname = request.form['classes']
-        fname="mooc.csv"
-        handle=open(fname,"w")
-        line='"'+"地点"+'","'+"时间"+'","'+"学号"+'","'+"班级"+'","'+"课程号"+'","'+"课序号"+'","'+"课程名"+'"\n'
-        handle.writelines(line)
+        flag=False
         for tr in table.find_all("tr"):
             if tr.find_all("td")[4].text==classname:
-                area=tr.find_all("td")[1].text
-                time=tr.find_all("td")[2].text
-                sno=tr.find_all("td")[3].text
-                classes=tr.find_all("td")[4].text
-                cnum=tr.find_all("td")[5].text
-                cno=tr.find_all("td")[6].text
-                cname=tr.find_all("td")[7].text
-                print("%s\n%s\n%s\n%s\n%s\n%s\n%s" % (area,time,sno,classes,cnum,cno,cname))
-                cursor.execute("insert into mooc (area,time,sno,classes,cnum,cno,cname) values (?, ?, ?,?,?,?,?)", (area,time,sno,classes,cnum,cno,cname))
-                line='"'+area+'","'+time+'","'+sno+'","'+classes+'","'+cnum+'","'+cno+'","'+cname+'"\n'
-                handle.writelines(line)  
-            con.commit()
-        handle.close()
-        classlist=['软件1510','软件1511','软件1512',
-           '软件1513','软件1514','软件1515','软件1516']
-        nums=[0,0,0,0,0,0,0]
-        for i in range(0,7):
+                flag=True
+        if flag ==False:
+            return render_template('validator.html')
+        else:  
+            con = sqlite3.connect("mooc.db")
+            cursor = con.cursor()
+            cursor.execute("drop table if exists mooc")
+            cursor.execute("create table if not exists mooc (id integer primary key,area text,time text,sno text,classes text,cnum text,cno text,cname text)")
+            fname="mooc.csv"
+            handle=open(fname,"w")
+            line='"'+"地点"+'","'+"时间"+'","'+"学号"+'","'+"班级"+'","'+"课程号"+'","'+"课序号"+'","'+"课程名"+'"\n'
+            handle.writelines(line)
             for tr in table.find_all("tr"):
-                if tr.find_all("td")[4].text==classlist[i]:
-                    nums[i]+=1
-        fig,ax=plt.subplots()
-        ax.plot(classlist,nums)
-        ax.grid()
-        fig.savefig("1510-1516尔雅考试人数.png")
-        con.close()
-    return render_template('finish.html')
+                if tr.find_all("td")[4].text==classname:
+                    area=tr.find_all("td")[1].text
+                    time=tr.find_all("td")[2].text
+                    sno=tr.find_all("td")[3].text
+                    classes=tr.find_all("td")[4].text
+                    cnum=tr.find_all("td")[5].text
+                    cno=tr.find_all("td")[6].text
+                    cname=tr.find_all("td")[7].text
+                    print("%s\n%s\n%s\n%s\n%s\n%s\n%s" % (area,time,sno,classes,cnum,cno,cname))
+                    cursor.execute("insert into mooc (area,time,sno,classes,cnum,cno,cname) values (?, ?, ?,?,?,?,?)", (area,time,sno,classes,cnum,cno,cname))
+                    line='"'+area+'","'+time+'","'+sno+'","'+classes+'","'+cnum+'","'+cno+'","'+cname+'"\n'
+                    handle.writelines(line)  
+                con.commit()
+            handle.close()
+            classlist=['软件1510','软件1511','软件1512',
+               '软件1513','软件1514','软件1515','软件1516']
+            nums=[0,0,0,0,0,0,0]
+            for i in range(0,7):
+                for tr in table.find_all("tr"):
+                    if tr.find_all("td")[4].text==classlist[i]:
+                        nums[i]+=1
+            fig,ax=plt.subplots()
+            ax.plot(classlist,nums)
+            ax.grid()
+            fig.savefig("1510-1516尔雅考试人数.png")
+            con.close()
+        return render_template('finish.html')
 
     
     
